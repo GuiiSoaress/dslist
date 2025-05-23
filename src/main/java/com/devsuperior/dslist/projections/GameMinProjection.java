ce2d6d453f26771027618722ef865cbd2dabc6e3
Guilherme Soares <guihhsoaress@gmail.com>
2025-05-23 18:07:30 -0300

    Sql query projection

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslist.projections;
+
+public interface GameMinProjection {
+
+	Long getId();
+	String getTitle();
+	Integer getGame_year();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+	
+}
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -7,10 +7,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameDTO;
-import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
-import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Component
@@ -32,4 +31,9 @@ public class GameService {
 		return dto;
 	}
 	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
 }
